e71760560d72ad9705cc10277fa2f3860a49c4d9
Martin Møller <mmj@dbc.dk>
2015-08-12 10:07:07 +0200

    - updated tests - updated dependencies + .eslintrc - added .editorconfig - updated README

---
+++ wallaby.config.js
@@ -0,0 +1,27 @@
+'use strict';
+let babel = require('babel');
+
+module.exports = function() {
+  return {
+    files: [
+      {pattern: 'src/*.js'},
+    ],
+
+    tests: [
+      {pattern: 'src/**/*.test.js'}
+    ],
+
+    preprocessors: {
+      '**/*.js': [
+        file => babel.transform(file.content, {sourceMaps: true})
+      ]
+    },
+
+    env: {
+      type: 'node',
+      runner: 'node'
+    },
+
+    testFramework: 'mocha@2.1.0'
+  };
+};
